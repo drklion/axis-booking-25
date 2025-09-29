da221645130ca57cdd874d217119db435ba05ecd
drklion <drklion@mac.com>
2025-09-29 08:27:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import ErrorBoundary from "./ErrorBoundary.jsx";
-import "react-datepicker/dist/react-datepicker.css";
 
-createRoot(document.getElementById("root")).render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>
-);
+createRoot(document.getElementById("root")).render(<App />);
