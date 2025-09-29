49a660367c0647e6c8a0cbb43011fd7ca97916c8
drklion <drklion@mac.com>
2025-09-29 09:57:41 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
-import "./index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+
+// LOAD YOUR PLAIN CSS (this is the bit that was missing)
+import "./index.css";
+
+// Keep datepicker CSS
+import "react-datepicker/dist/react-datepicker.css";
 
 createRoot(document.getElementById("root")).render(<App />);
